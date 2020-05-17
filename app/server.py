b46c9c617545ebecb18869be90cc26724e50da44
NarendraBabuOggu <narendrababuoggu393@gmail.com>
2020-05-17 10:48:12 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -21,19 +21,7 @@ app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
 app.mount('/static', StaticFiles(directory='app/static'))
 
-
-"""
-async def download_file(url, dest):
-    if dest.exists(): return
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.read()
-            with open(dest, 'wb') as f:
-                f.write(data)
-"""
-
 async def setup_learner():
-    #await download_file(export_file_url, path / export_file_name)
     try:
         print("Trying to Load Learner")
         defaults.device = torch.device('cpu')
